cab8fc88ec9bed437de638179d1660109db3c421
TYA_IOS <appadmin@segio.com>
2024-09-27 14:21:09 +0900

    Initial Commit

---
+++ SaveWrite/SaveWriteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveWriteApp.swift
+//  SaveWrite
+//
+//  Created by NewUser on 9/27/24.
+//
+
+import SwiftUI
+
+@main
+struct SaveWriteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
